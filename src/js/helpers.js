241cc7ed8aea1562a604983bfb1f3f19e7f4d7f9
Benoit Rajalu <rajalubenoit@gmail.com>
2022-04-04 17:55:29 +0200

    feat(Paths): Clean path for automation

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,1 +1,7 @@
 /** Helpers are empty for now. All path generation for this exporter is done purely through the path blueprint */
+Pulsar.registerFunction("logKeys", function (object) {
+    console.log(Object.keys(object));
+    for (const entry in object) {
+      console.log(entry);
+    }
+  });
